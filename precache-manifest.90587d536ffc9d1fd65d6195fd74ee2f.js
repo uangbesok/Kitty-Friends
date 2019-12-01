8021f532f40c125d6e4dd2b2d20692a0dc824f99
uangbesok <Straus@Marinas-MacBook-Pro.local>
2019-12-01 18:20:55 +0800

    Updates

---
+++ precache-manifest.90587d536ffc9d1fd65d6195fd74ee2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d024ad556394c0ac53119d7ab6d3325",
+    "url": "/Kitty-Friends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/Kitty-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "570b7fc51b6234cd882f",
+    "url": "/Kitty-Friends/static/css/main.695188f8.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/Kitty-Friends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "570b7fc51b6234cd882f",
+    "url": "/Kitty-Friends/static/js/main.6f5b8dfd.chunk.js"
+  },
+  {
+    "revision": "3eb45323e0a308825553",
+    "url": "/Kitty-Friends/static/js/runtime-main.5fafebde.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Kitty-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
