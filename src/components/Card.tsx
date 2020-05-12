c397af56614a7bf4e7dcbc7a091ac0e5dc3028bd
uangbesok <45453585+uangbesok@users.noreply.github.com>
2020-05-12 22:40:41 +0800

    Add typescript support to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+export interface CardProps {
+  id : number;
+  name: number;
+  email: boolean;
+}
+
 // Displays generated kitty
-const Card = ({id, name, email}) => {
+const Card : React.FC<CardProps>  = ({id, name, email}) => {
   return (
     <div className="bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc">
       <img src={`https://robohash.org/${id}?set=set4&size=200x200`} alt="" />
